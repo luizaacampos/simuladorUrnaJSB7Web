8d05ab30d3d8f8e2d4f7f28c3a5afbe829e8c1db
Luiza Campos <luiza.almcampos@gmail.com>
2020-11-09 14:43:03 -0300

    Urna Eletronica

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Alfred Pennyworth',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'alfred.jpg', legenda: "Vereador", small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Yoshi',
+                partido: 'DEFG',
+                fotos: [
+                    {url: 'yoshi.png', legenda: "Vereador", small: false}
+                ] 
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome:'Mario Bros',
+                partido: 'ABC',
+                vice: 'Luigi Bros',
+                fotos: [
+                    {url: 'mario.png',legenda: 'Prefeito'},
+                    {url: 'luigi.png', legenda: 'Vice-prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome:'Batman',
+                partido: 'QWERTY',
+                vice: 'Robin',
+                fotos: [
+                    {url: 'batman.png',legenda: 'Prefeito'},
+                    {url: 'robin.png', legenda: 'Vice-prefeito', small: true}
+                ]
+            }
+        ]
+    }
+]
